f96219fab55774e7f02eb1eeda7e456cb2bb51a5
ying.huang11 <ying.huang11@zhenai.com>
2024-06-25 09:48:53 +0800

    feat: common style

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Redirect } from "react-router-dom";
-import Home from '../application/Home';
-import Recommend from '../application/Recommend';
-import Singers from '../application/Singers';
-import Rank from '../application/Rank';
+import Home from '@/application/Home';
+import Recommend from '@/application/Recommend';
+import Singers from '@/application/Singers';
+import Rank from '@/application/Rank';
 
 export default [
   {
