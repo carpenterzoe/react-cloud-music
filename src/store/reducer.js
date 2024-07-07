6cc39c8c6d1c1970d952eaa0d2c57c035e73345c
carpenterzoe <carpenterzoe@outlook.com>
2024-07-07 09:53:54 +0800

    feat: recommend reducer 注册到全局 store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux-immutable';
+import { reducer as recommendReducer } from '@/application/Recommend/store/index';
 
+// 将 recommend 的 reducer 注册到全局 store
 export default combineReducers ({
-// 之后开发具体功能模块的时候添加 reducer
+  recommend: recommendReducer,
 });
