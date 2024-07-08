8da3e26bd643d01dd2e8f246ea3d7f69c0ba8505
ying.huang11 <ying.huang11@zhenai.com>
2024-07-08 09:58:04 +0800

    feat: 分类点击高亮

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Horizen from '@/baseUI/horizen-item';
 import { categoryTypes, alphaTypes } from '@/api/config';
 import { NavContainer } from "./style";
 
 function Singers () {
+  let [category, setCategory] = useState ('');
+  let [alpha, setAlpha] = useState ('');
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha (val);
+  }
+
+  let handleUpdateCatetory = (val) => {
+    setCategory (val);
+  }
+
   return (
     <NavContainer>
-      <Horizen list={categoryTypes} title={"分类 (默认热门):"}></Horizen>
-      <Horizen list={alphaTypes} title={"首字母:"}></Horizen>
+      <Horizen 
+        list={categoryTypes} 
+        title={"分类 (默认热门):"} 
+        handleClick={val => handleUpdateCatetory(val)} 
+        oldVal={category}>
+      </Horizen>
+      <Horizen 
+        list={alphaTypes} 
+        title={"首字母:"} 
+        handleClick={val => handleUpdateAlpha (val)} 
+        oldVal={alpha}>
+      </Horizen>
     </NavContainer>
   )
 }
